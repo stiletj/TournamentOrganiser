8b78c1c8d54ee9e7e9523d1e822795553961749f
stiletj <tjstiles440@gmail.com>
2024-01-03 13:40:14 +0800

    Initial Commit

---
+++ TournamentOrganiser.cpp
@@ -0,0 +1,15 @@
+// TournamentOrganiser.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+	if (argc > 1)
+	{
+		return 1;
+	}
+
+	std::cout << "Hello World" << std::endl;
+	return 0;
+}
